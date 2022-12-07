149b74308bdfecf053e14a8b784d49ad25fb5d32
sunny-test <dextrob7@gmail.com>
2022-12-07 16:27:24 +0530

    modify routing file and add 5 api's

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('reset', function () {
+    Artisan::call('route:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+});
+
+
+Route::post('is-user-exist', 'App\Http\Controllers\Api_V1\UserController@isUserExist');
